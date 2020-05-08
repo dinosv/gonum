b15abeb869cad53853c4187c4e6a24b706a5b7a6
Vladimir Chalupecky <vladimir@chalupecky.dev>
2020-05-08 10:52:52 +0200

    lapack/gonum: add Dlantb

---
+++ lapack/testlapack/dlantb.go
@@ -0,0 +1,14 @@
+// Copyright ©2020 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package testlapack
+
+import (
+	"gonum.org/v1/gonum/blas"
+	"gonum.org/v1/gonum/lapack"
+)
+
+type Dlantber interface {
+	Dlantb(norm lapack.MatrixNorm, uplo blas.Uplo, diag blas.Diag, n, k int, a []float64, lda int, work []float64) float64
+}
